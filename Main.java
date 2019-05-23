da3372e7b3703143716b47b4ef1d068f07483c76
Stefun Hawkins <stefunhawkins@zipcoders-MacBook-Pro-8.local>
2019-05-23 15:11:07 -0400

    Bout time

--- Main.java
+++ Main.java
@@ -5,18 +5,28 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Random;
+import java.util.Scanner;
 public class Main
 {
     // instance variables - replace the example below with your own
-    private int x;
-
+    private int randNum;
+    private int guesses;
+    private int userNum;
+    private int currGuess;
+    private int prevGuess;
     /**
      * Constructor for objects of class Main
      */
     public Main()
     {
+        
+        
         // initialise instance variables
-        x = 0;
+         
+        
+        
+        
     }
 
     /**
@@ -25,9 +35,38 @@ public class Main
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
-}
+    public void gameTime()
+     {
+         //put your code here
+         Random rand = new Random();
+        int randNum = rand.nextInt(30);
+        // initialise instance variables
+        prevGuess = 0;
+        guesses = 0;
+        randNum += 1; 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a positve whole number between 0 and 30");
+        
+        while(userNum != randNum){
+                userNum = in.nextInt();
+             
+                if(userNum < randNum && userNum != prevGuess){
+                    guesses++;
+                    prevGuess = userNum;
+                    System.out.println("Too small. Try again.");
+               
+               
+                }
+                else if(userNum > randNum && userNum != prevGuess){
+                    guesses++;
+                    prevGuess = userNum;
+                    System.out.println("Too large. Try again");
+                
+                }
+                
+        
+    };
+        guesses++;
+        
+        System.out.println("Good job! It only took you " + guesses + " guesses!");
+    }}
